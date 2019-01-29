bb16846ed7a6d7ad38fee224e2b2186db5b77f2b
Ben Watson <ben.watson.developer@gmail.com>
2019-01-28 22:57:01 -0500

    completes 07

--- DistanceConversions.hs
+++ DistanceConversions.hs
@@ -1,17 +1,18 @@
 module DistanceConversions
 ( yardsToFeet
 , feetToInches
-, inchesToCentimetres
+, inchesToCentimeters
 ) where
 
 -- Define yards to feet
 yardsToFeet ::  Float -> Float
-yardsToFeet y = undefined
+yardsToFeet y = y * 3
 
 -- Define feet to inches
 feetToInches :: Float -> Float
-feetToInches f = undefined
+feetToInches f = f * 12
 
--- Define inches to centimetres
-inchesToCentimetres :: Float -> Float
-inchesToCentimetres i = undefined
+-- Define inches to centimeters
+inchesToCentimeters :: Float -> Float
+inchesToCentimeters i = i * 2.54
+
